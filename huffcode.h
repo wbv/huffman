e2fef80c09dbeab0656feeb601cc5e35a9f35728
Walter Vaughan <wbv@users.noreply.github.com>
2018-10-20 20:09:00 -0600

    added more documentation (doxygen)

--- huffcode.h
+++ huffcode.h
@@ -12,28 +12,32 @@ using std::uint32_t;
 using std::ifstream;
 using std::ofstream;
 
+/// represents a single Huffman code point, up to 16 bits long
 struct huffcode_t {
+	/// indicates how many bits are in the code
 	uint8_t bitcnt;
-	uint16_t bits;
-};
+	/// contains the bits actually in the Huffman code point.
+	/// The first bit of the huffman code is stored most significant, and the
+	/// last bit in the least significant bit of `bits`.
+	uint16_t bits; };
 
-// takes a histogram, makes a heap, then turns the heap into a tree for parsing
-// into a huffman code table
+/// takes a histogram, makes a heap, then turns the heap into a tree for parsing
+/// into a huffman code table
 node* getTreeFromHist(uint32_t hist[256]);
 
-// recursive function that will clean up the huffman code tree structure after
-// you're done with it.
+/// recursive function that will clean up the huffman code tree structure after
+/// you're done with it.
 void cleanTree(node* n);
 
-// fills the supplied huffmap from a tree, returns false on failure
+/// fills the supplied huffmap from a tree, returns false on failure
 void getHuffMapFromTree(huffcode_t* map, node* root, uint8_t bits = 0, uint16_t bitcnt = 0);
 
-// given an array which maps bytes to huffman codes, read from fin (start at 0)
-// and write out to fout (starting where it was left at)
+/// given an array which maps bytes to huffman codes, read from fin (start at 0)
+/// and write out to fout (starting where it was left at)
 void writeHuffman(huffcode_t huffmap[256], ifstream& fin, ofstream& fout);
 
-// given the code tree for huffman, read code from fin (starting where it was
-// left at) and write out the actual byte to fout (start at 0)
+/// given the code tree for huffman, read code from fin (starting where it was
+/// left at) and write out the actual byte to fout (start at 0)
 void readHuffman(node* root, ifstream& fin, ofstream& fout);
 
 #endif
--- minheap.h
+++ minheap.h
@@ -5,23 +5,38 @@
 
 #include "node.h"
 
-/* min-heap of static size, holding a weight and associated character */
-/* sorted on the weight of the associated character */
+/// min-heap of static size, holding a weight and associated character
+/// sorted on the weight of the associated character. max size is 256 (which is
+/// all that's needed for this program)
 class minheap
 {
-public:
+public:	
+	/// constructor initializes empty heap
 	minheap();
+	/// destructor doesn't have a job but needs a paycheck
 	~minheap();
 
+	/// insert a dynamically-allocated node into the statically-stored minheap,
+	/// deallocating it after storing it
 	void insert(node*);
+	/// insert a node into the heap from literal values
 	void insert(uint32_t, uint8_t);
+	/// default heap-remove.
 	node* pop_smallest();
+	/// read-only indicator of how many items are in the heap
 	size_t size();
 
 private:
+	/// primary storage mechanism for the heap. index 0 isn't treated as a spot
+	/// in the heap, instead being used for temporary storage. this allows the
+	/// children of a node at index `n` to be stored at `2n` and `2n+1`, all the
+	/// way up to the max capacity of 256 
 	node array[257]; /* an empty slot + 256 slots */
+
+	/// keeps track of how many items are in the heap
 	size_t pos;
 
+	/// used to rebuild the heap after a single item is removed from the root
 	void heapify(size_t);
 
 };
